ea706c85b96701513cbaa7cbcbb8f25fb3a9f203
Suraj kumar <62923466+Surajkumar98012@users.noreply.github.com>
2023-12-27 22:33:20 +0530

    Update mavenJarPipeline.groovy

--- vars/mavenJarPipeline.groovy
+++ vars/mavenJarPipeline.groovy
@@ -14,8 +14,8 @@ def call(Map pipelineParams) {
             }
             stage('Push') {
                 steps {
-                    sh "docker build -t ${pipelineParams.dockerHubUsername}/${pipelineParams.dockerImageName}:${pipelineParams.tag} ."
-                    sh "docker push ${pipelineParams.dockerHubUsername}/${pipelineParams.dockerImageName}:${pipelineParams.tag}"
+                    sh "sudo docker build -t ${pipelineParams.dockerHubUsername}/${pipelineParams.dockerImageName}:${pipelineParams.tag} ."
+                    sh "sudo docker push ${pipelineParams.dockerHubUsername}/${pipelineParams.dockerImageName}:${pipelineParams.tag}"
                 }
             }
         }
