e628030fc3b0487fe537138557d69f5bc557a942
Suraj kumar <62923466+Surajkumar98012@users.noreply.github.com>
2023-12-28 10:11:17 +0530

    Update mavenJarPipeline.groovy

--- vars/mavenJarPipeline.groovy
+++ vars/mavenJarPipeline.groovy
@@ -15,7 +15,7 @@ def call(Map pipelineParams) {
             stage('Push') {
                 steps {
                     withCredentials([usernamePassword(credentialsId: "${pipelineParams.dockerHubCredentials}", usernameVariable: 'DOCKERHUB_USERNAME', passwordVariable: 'DOCKERHUB_PASSWORD')]) {
-                        sh "docker login --username $DOCKERHUB_USERNAME --password $DOCKERHUB_PASSWORD"
+                        sh "docker login --username $DOCKERHUB_USERNAME --password-stdin <<< $DOCKERHUB_PASSWORD"
                         sh "docker build -t ${pipelineParams.dockerHubUsername}/${pipelineParams.dockerImageName}:${pipelineParams.tag} ."
                         sh "docker push ${pipelineParams.dockerHubUsername}/${pipelineParams.dockerImageName}:${pipelineParams.tag}"
                     }
